9e873fd429946cac7c440c89cb54866e3b654f2d
Vitaly Pronkin <vpronkin@inmusicbrands.com>
2025-01-03 21:51:22 +1000

    fix unnecessary updates of text widgets

--- Sources/Controllers/Map/Widgets/OATextInfoWidget.mm
+++ Sources/Controllers/Map/Widgets/OATextInfoWidget.mm
@@ -562,7 +562,7 @@ static NSString * _Nonnull const kSizeStylePref = @"simple_widget_size";
 
 - (void) setTextNoUpdateVisibility:(NSString *)text subtext:(NSString *)subtext
 {
-    if ([_text isEqualToString:text] && [_subtext isEqualToString:subtext])
+	if (([_text isEqualToString:text] || (_text == text)) && ([_subtext isEqualToString:subtext] || _subtext == subtext))
         return;
     if (text.length == 0 && subtext.length == 0)
     {
